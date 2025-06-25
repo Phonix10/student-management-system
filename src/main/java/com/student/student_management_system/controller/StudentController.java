035c5de4b67b2e27ec61a87e86bbb9d6e25d41d2
Udit Ranjan <101802436+Phonix10@users.noreply.github.com>
2025-06-25 23:32:03 +0530

    config and postmapping in controller added working fine

--- src/main/java/com/student/student_management_system/controller/StudentController.java
+++ src/main/java/com/student/student_management_system/controller/StudentController.java
@@ -1,8 +1,33 @@
 package com.student.student_management_system.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.student.student_management_system.entity.Student;
+import com.student.student_management_system.service.StudentService;
+
 @RestController
+@RequestMapping("/api/students")
 public class StudentController {
+
+    @Autowired
+    private StudentService studentService;
+
+    
+    
+    @PostMapping()
+    public ResponseEntity<Student> registerStudent(@RequestBody Student student) {
+        Student created = studentService.addStudent(student);
+        return new ResponseEntity<>(created, HttpStatus.CREATED);
+    }
+
+
+
+
     
 }
