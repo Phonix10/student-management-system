a790dff30bbb2b3e33f30649e2adba59f6f05308
Udit Ranjan <101802436+Phonix10@users.noreply.github.com>
2025-06-25 23:54:05 +0530

    complete porject without security

--- src/main/java/com/student/student_management_system/controller/StudentController.java
+++ src/main/java/com/student/student_management_system/controller/StudentController.java
@@ -1,9 +1,15 @@
 package com.student.student_management_system.controller;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -19,15 +25,39 @@ public class StudentController {
     private StudentService studentService;
 
     
-    
+    // Endpoint to register a new student 
     @PostMapping()
     public ResponseEntity<Student> registerStudent(@RequestBody Student student) {
         Student created = studentService.addStudent(student);
         return new ResponseEntity<>(created, HttpStatus.CREATED);
     }
 
+    // Endpoint to get all students 
+    @GetMapping
+    public ResponseEntity<List<Student>> getAllStudents() {
+        List<Student> students = studentService.getAllStudents();
+        return new ResponseEntity<>(students, HttpStatus.OK);
+    }
 
+    // Endpoint to get a student by ID
+    @GetMapping("/{id}")
+    public ResponseEntity<Student> getStudentById(@PathVariable Long id) {
+        Student student = studentService.getStudentById(id);
+        return new ResponseEntity<>(student, HttpStatus.OK);
+    }
 
+    // Endpoint to Edit a student by ID
+    @PutMapping("/{id}")
+    public ResponseEntity<Student> updateStudent(@PathVariable Long id, @RequestBody Student student) {
+        Student updated = studentService.updateStudent(id, student);
+        return new ResponseEntity<>(updated, HttpStatus.OK);
+    }
+    // Endpoint to delete a student by ID
+     @DeleteMapping("/{id}")
+    public ResponseEntity<String> deleteStudent(@PathVariable Long id) {
+        studentService.deleteStudent(id);
+        return new ResponseEntity<>("Student deleted successfully", HttpStatus.NO_CONTENT);
+    }
 
     
 }
